ec89e544b2a61eef04f052f79a6dd62af57ebe1a
Simo64Terminator <simo64.tomasella@gmail.com>
2024-05-07 11:04:28 +0200

    Small changes

--- scenes/ui/menus/main_menu.gd
+++ scenes/ui/menus/main_menu.gd
@@ -1,7 +1,8 @@
 extends Control
 
 @onready var start_game_button: Button = $MarginContainer/VBoxContainer/ButtonsVBox/StartGameButton
-@onready var buttons_v_box: VBoxContainer = $MarginContainer/VBoxContainer/ButtonsVBox
+@onready var audioPlayer: AudioStreamPlayer = $AudioStreamPlayer
+
 signal start_game()
 
 func _ready():
@@ -11,12 +12,11 @@ func _on_start_game_button_pressed() -> void:
 	start_game.emit()
 	hide()
 
+func _on_credits_button_pressed() -> void:
+	print("Palle")
+
 func _on_exit_game_button_pressed() -> void:
 	get_tree().quit()
 
 func focus_button() -> void:
 	start_game_button.grab_focus()
-	#if buttons_v_box:
-	#	var button: Button = buttons_v_box.get_child(0)
-	#	if button is Button:
-	#		button.grab_focus()
