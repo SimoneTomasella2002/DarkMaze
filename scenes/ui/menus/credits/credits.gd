8b7b088f1121152911bb2ee182e5128f5eeac308
Simo64Terminator <simo64.tomasella@gmail.com>
2024-09-15 22:46:01 +0200

    Added credits scene and menu button

---
+++ scenes/ui/menus/credits/credits.gd
@@ -0,0 +1,17 @@
+extends RichTextLabel
+
+
+func _ready() -> void:
+	scroll_text(text)
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("menu"):
+		get_tree().change_scene_to_file("res://scenes/ui/menus/main_menu/main_menu.tscn")
+
+
+func scroll_text(input_text:String) -> void:
+	visible_characters = 0
+	
+	for i in get_parsed_text():
+		visible_characters += 1
+		await get_tree().create_timer(0.1).timeout
--- scenes/ui/menus/main_menu/main_menu.gd
+++ scenes/ui/menus/main_menu/main_menu.gd
@@ -3,17 +3,22 @@ extends Control
 @onready var start_game_button: Button = $MarginContainer/VBoxContainer/ButtonsVBox/StartGameButton
 @onready var audioPlayer: AudioStreamPlayer = $AudioStreamPlayer
 
+
 signal start_game()
 
 func _ready():
 	focus_button()
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("menu"):
+		get_tree().quit()
 
 func _on_start_game_button_pressed() -> void:
 	start_game.emit()
 	hide()
 
 func _on_credits_button_pressed() -> void:
-	print("Palle")
+	get_tree().change_scene_to_file("res://scenes/ui/menus/credits/credits.tscn")
 
 func _on_exit_game_button_pressed() -> void:
 	get_tree().quit()
