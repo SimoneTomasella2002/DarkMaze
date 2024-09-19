c39896e360cd56deb039c220ec7dc5936063c875
WCala <cala.2002.marco@gmail.com>
2024-09-19 17:36:28 +0200

    Update player.gd with adjustments and the addition of the FRICTION
    La FRICTION serve per dare più realismo al movimento del player nel momento in cui prova a fermarsi (rallenta invece di bloccarsi subito).

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,17 +3,25 @@ extends CharacterBody2D
 # Const and variables
 const SPEED = 300.0
 const ACCEL = 2.0
+const FRICTION = 100.0
 
-var input: Vector2
-
+var input: Vector2.ZERO
+var velocity: Vector2.ZERO
 
 # Default functions
 
 func _process(delta: float) -> void:
 	var playerInput = get_input()
-	velocity = lerp(velocity, playerInput * SPEED, delta * ACCEL)
-	
-	move_and_slide()
+	// velocity = lerp(velocity, playerInput * SPEED, delta * ACCEL)
+
+	if playerInput != Vector2.ZERO:
+		velocity = velocity.move_toward(playerInput * SPEED, ACCEL * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity = move_and_slide(velocity)
+
+	// move_and_slide()
 
 
 # Custom functions
