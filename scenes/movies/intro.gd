bce6ba7a3ef09091d7fbe54f78141527df8d5cb7
Simo64Terminator <simo64.tomasella@gmail.com>
2024-09-21 12:29:35 +0200

    Fixed double esc crashing bug and improved credits and intro screens

--- scenes/movies/intro.gd
+++ scenes/movies/intro.gd
@@ -1,14 +1,19 @@
 extends Control
 
 @onready var intro_text = $Background/IntroText
+@onready var press_start_prompt = $Background/StartPromptText
 @onready var press_start_animation_player = $Background/StartPromptText/AnimationPlayer
 
+var is_scrolling = true
+
 func _ready() -> void:
+	press_start_prompt.visible = false
 	scroll_text()
-	press_start_animation_player.play("fade-in_fade-out")
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("menu"):
+	if (is_scrolling == true) and Input.is_action_just_pressed("menu"):
+		show_press_button_prompt()
+	elif Input.is_action_just_pressed("menu"):
 		FadeTransition.load_scene("res://scenes/levels/level1.tscn")
 
 func scroll_text() -> void:
@@ -16,5 +21,14 @@ func scroll_text() -> void:
 	var total_characters = intro_text.get_total_character_count()
 	
 	for i in range(total_characters):
-		intro_text.visible_characters = i + 1
-		await get_tree().create_timer(0.1).timeout
+		if (is_scrolling):
+			intro_text.visible_characters = i + 1
+			await get_tree().create_timer(0.1).timeout
+		else:
+			intro_text.visible_characters = total_characters
+			break
+
+func show_press_button_prompt() -> void:
+	is_scrolling = false
+	press_start_prompt.visible = true
+	press_start_animation_player.play("fade-in_fade-out")
--- scenes/ui/menus/credits/credits.gd
+++ scenes/ui/menus/credits/credits.gd
@@ -1,20 +1,34 @@
 extends Control
 
-@onready var credits_text = $Background/RichTextLabel
-@onready var press_start_animation_player = $Background/pressStartLabel/AnimationPlayer
+@onready var credits_text = $Background/CreditsText
+@onready var press_start_prompt = $Background/StartPromptText
+@onready var press_start_animation_player = $Background/StartPromptText/AnimationPlayer
+
+var is_scrolling = true
 
 func _ready() -> void:
+	press_start_prompt.visible = false
 	scroll_text()
-	press_start_animation_player.play("fade-in_fade-out")
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("menu"):
+	if (is_scrolling == true) and Input.is_action_just_pressed("menu"):
+		show_press_button_prompt()
+	elif Input.is_action_just_pressed("menu"):
 		FadeTransition.load_scene("res://scenes/ui/menus/main_menu/main_menu.tscn")
 
 func scroll_text() -> void:
 	credits_text.visible_characters = 0
-	var total_characters = credits_text.get_total_character_count()  # Ottiene il numero totale di caratteri
+	var total_characters = credits_text.get_total_character_count()
 	
-	for i in range(total_characters):  # Cicla attraverso il numero totale di caratteri
-		credits_text.visible_characters = i + 1  # Aumenta il numero di caratteri visibili uno alla volta
-		await get_tree().create_timer(0.1).timeout  # Aggiunge un ritardo di 0.1 secondi per ogni carattere
+	for i in range(total_characters):
+		if (is_scrolling):
+			credits_text.visible_characters = i + 1
+			await get_tree().create_timer(0.1).timeout
+		else:
+			credits_text.visible_characters = total_characters
+			break
+
+func show_press_button_prompt() -> void:
+	is_scrolling = false
+	press_start_prompt.visible = true
+	press_start_animation_player.play("fade-in_fade-out")
