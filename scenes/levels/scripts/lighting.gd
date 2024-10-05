a5558259c73e081454fb19bebde4f6f94fd9c32a
SimoneTomasella <simo64.tomasella@gmail.com>
2024-10-05 15:57:32 +0200

    Added lighting system, broke pause menu (need refactoring), WIP on wall_torches system

---
+++ scenes/levels/scripts/lighting.gd
@@ -0,0 +1,17 @@
+extends ColorRect
+
+
+func _ready():
+	show()
+
+
+func _process(delta):
+	var light_positions = _get_light_positions()
+	material.set_shader_parameter("number_of_lights", light_positions.size())
+	material.set_shader_parameter("lights", light_positions)
+
+func _get_light_positions():
+	return get_tree().get_nodes_in_group("light").map(
+		func(light: Node2D):
+			return light.get_global_transform_with_canvas().origin
+	)
